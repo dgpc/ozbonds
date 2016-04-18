e1bacdc29087649dc9285d72e38ea105fba7e463
David Claridge <daave@daave.com>
2016-04-18 13:44:34 +1000

    Set up handler for ACME challenges from Let's Encrypt

--- app.go
+++ app.go
@@ -1,11 +1,23 @@
 package ozbonds
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"path"
+)
 
 func init() {
-	http.HandleFunc("/", notFoundHandler)
+	http.HandleFunc("/.well-known/acme-challenge/", letsEncryptHandler)
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
+func letsEncryptHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print(path.Base(r.URL.Path))
+	switch path.Base(r.URL.Path) {
+	case "23evrIwkgjfAF7dfgtDYHMLe0X9fSOb1RQ-0-LCDnEs":
+		w.Write([]byte("23evrIwkgjfAF7dfgtDYHMLe0X9fSOb1RQ-0-LCDnEs.bVSWcev0D89-Az2rCn-7O6lvgP17vsd0TjC30rFfyFg"))
+	case "pKvAcAeSHjday7esPL0Dj8QiAUUYY3TdLfHKDXcFDXo":
+		w.Write([]byte("pKvAcAeSHjday7esPL0Dj8QiAUUYY3TdLfHKDXcFDXo.bVSWcev0D89-Az2rCn-7O6lvgP17vsd0TjC30rFfyFg"))
+	default:
+		http.NotFound(w, r)
+	}
 }

379bf300ec28ebee0668fda78ff6b7e75d6161cc
David Claridge <daave@daave.com>
2016-04-18 14:15:17 +1000

    Add additional Let's Encrypted challenge responses

--- app.go
+++ app.go
@@ -17,6 +17,10 @@ func letsEncryptHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("23evrIwkgjfAF7dfgtDYHMLe0X9fSOb1RQ-0-LCDnEs.bVSWcev0D89-Az2rCn-7O6lvgP17vsd0TjC30rFfyFg"))
 	case "pKvAcAeSHjday7esPL0Dj8QiAUUYY3TdLfHKDXcFDXo":
 		w.Write([]byte("pKvAcAeSHjday7esPL0Dj8QiAUUYY3TdLfHKDXcFDXo.bVSWcev0D89-Az2rCn-7O6lvgP17vsd0TjC30rFfyFg"))
+	case "7tSbkqsduCTxQjuFXzPSPolZe1yZhhEylHVUUlGH4Z8":
+		w.Write([]byte("7tSbkqsduCTxQjuFXzPSPolZe1yZhhEylHVUUlGH4Z8.DtQyDZ-Ecmb9ThI_Itv4Xb4j5bYstW4vG0Aloy6LMSY"))
+	case "tC8mM1eJqJK5u0aU7qHg7ugc4mZrznu3fXeiOvG4mr0":
+		w.Write([]byte("tC8mM1eJqJK5u0aU7qHg7ugc4mZrznu3fXeiOvG4mr0.DtQyDZ-Ecmb9ThI_Itv4Xb4j5bYstW4vG0Aloy6LMSY"))
 	default:
 		http.NotFound(w, r)
 	}
